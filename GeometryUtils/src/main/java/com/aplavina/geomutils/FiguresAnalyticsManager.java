434b8af8d6d6f71239341b531bdc22d38fff31cb
AnnaAplavina <59871896+AnnaAplavina@users.noreply.github.com>
2025-02-24 09:56:34 +0300

    Utils version 1.1.0-SNAPSHOT

--- GeometryUtils/src/main/java/com/aplavina/geomutils/FiguresAnalyticsManager.java
+++ GeometryUtils/src/main/java/com/aplavina/geomutils/FiguresAnalyticsManager.java
@@ -7,5 +7,6 @@ import java.util.List;
 public class FiguresAnalyticsManager {
     public static void printAnalytics(List<Figure> figures) {
         System.out.println("There are " + figures.size() + " figures");
+        System.out.println("Total area: " + figures.stream().mapToDouble(Figure::area).sum());
     }
 }
